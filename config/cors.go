e1b28469ebe37ccaa104ffb8a8af31438ad88df9
mrestuab <mrestuab54@gmail.com>
2025-07-07 14:44:23 +0700

    done

--- config/cors.go
+++ config/cors.go
@@ -1,10 +1,10 @@
 package config
 
 var allowedOrigins = []string {
-	// "http://localhost:3000",
-	// "http://localhost:5173/#",
-	// "http://127.0.0.1:8088/",
-	// "https://frontend-restu.vercel.app/",
+	 "http://localhost:3000",
+	 "http://localhost:5173/#",
+	 "http://127.0.0.1:8088/",
+	 "https://frontend-restu.vercel.app/",
 }
 
 func GetAllowedOrigins() []string {
