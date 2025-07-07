b8f4488a10e201f58c5bb4a11f6d57a8d3f8d174
mrestuab <mrestuab54@gmail.com>
2025-07-07 14:35:51 +0700

    fix: update allowed origin URL to include scheme

--- config/cors.go
+++ config/cors.go
@@ -4,7 +4,7 @@ var allowedOrigins = []string {
 	"http://localhost:3000",
 	"http://localhost:5173/#",
 	"http://127.0.0.1:8088/",
-	"frontend-restu.vercel.app",
+	"https://frontend-restu.vercel.app/",
 }
 
 func GetAllowedOrigins() []string {
