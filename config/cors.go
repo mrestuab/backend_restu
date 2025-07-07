e7ab72bd24b8a517c5d6560402af14ca59e596ed
mrestuab <mrestuab54@gmail.com>
2025-07-07 14:30:47 +0700

    dploy fe

--- config/cors.go
+++ config/cors.go
@@ -4,6 +4,7 @@ var allowedOrigins = []string {
 	"http://localhost:3000",
 	"http://localhost:5173/#",
 	"http://127.0.0.1:8088/",
+	"frontend-restu.vercel.app",
 }
 
 func GetAllowedOrigins() []string {
