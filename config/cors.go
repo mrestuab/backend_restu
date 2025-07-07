1a468fd9b4d2cb41511ef7c174a170b286c45bbc
mrestuab <mrestuab54@gmail.com>
2025-07-07 14:40:01 +0700

    1

--- config/cors.go
+++ config/cors.go
@@ -1,12 +1,12 @@
-// package config
+package config
 
-// var allowedOrigins = []string {
-// 	"http://localhost:3000",
-// 	"http://localhost:5173/#",
-// 	"http://127.0.0.1:8088/",
-// 	"https://frontend-restu.vercel.app/",
-// }
+var allowedOrigins = []string {
+	// "http://localhost:3000",
+	// "http://localhost:5173/#",
+	// "http://127.0.0.1:8088/",
+	// "https://frontend-restu.vercel.app/",
+}
 
-// func GetAllowedOrigins() []string {
-// 	return allowedOrigins
-// }
+func GetAllowedOrigins() []string {
+	return allowedOrigins
+}
